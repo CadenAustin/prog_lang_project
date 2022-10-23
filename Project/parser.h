a7434c22f9d435673f77269886dcdd19e902f337
Caden Austin <cadenbryant@hotmail.com>
2022-10-22 22:18:02 -0500

    Midpoint

--- Project/parser.h
+++ Project/parser.h
@@ -1,8 +1,9 @@
-//**************************************************************************
- 
-// Replace with appropriate header comment......
-
-//**************************************************************************
+/********************************************************************* 
+  Name: Caden Austin             NetID: CBA169 
+  Course: CSE 4714              Assignment: Part 2 
+  Programming Environment: Linux/VSCODE
+  Purpose of File: Contains the Parser Header File
+*********************************************************************/ 
 
 #ifndef PARSER_H
 #define PARSER_H
@@ -26,5 +27,16 @@ extern "C" {
 
 // Function declarations
 void program();
+void block();
+void compound_stmt();
+void statement();
+void assignment_stmt();
+void if_stmt();
+void while_stmt();
+void read_stmt();
+void write_stmt();
+void expression();
+void simp_expr();
+void identifier_block();
 
 #endif /* PARSER_H */
